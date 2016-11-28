601dda339d86e8ec6617eb0c10550ad9b48471a7
Jonatan Norbye <jonatan@norbye.com>
2016-11-28 18:41:15 +0100

    Added registering and logging in through facebook

--- classes/login.class.php
+++ classes/login.class.php
@@ -1,20 +1,125 @@
 <?php
-if(class_exists("GM_Login")){
+if(!class_exists("GM_Login")){
 class GM_Login{
     function __construct(){
-        //??
+        require CONNECT_TO_DB;
+        $this->db = $db;
+        $this->tbl = $tbl;
+        
+        $this->error = array();
     }
     
     function fb(){
-        //Get info from user account
+        //Get info from user account and then register / login
+        $code = $_GET['code'];
+        $app_id = "553540261437395";
+        $app_secret = "c1773e735cf65bcf837b0286267744bd";
+        $my_url = "http://getmoving.norbye.com/?pid=".get_pid($this->db, $this->tbl, "handle_login");
+        
+        //Redirect if the code is not sent
+        if(empty($code)) {
+            $_SESSION['state'] = md5(uniqid(rand(), TRUE)); //CSRF protection
+            $dialog_url = "https://www.facebook.com/dialog/oauth?client_id=" 
+            . $app_id . "&redirect_uri=" . urlencode($my_url) . "&state="
+            . $_SESSION['state'];
+
+            die("<script> top.location.href='" . $dialog_url . "'</script>");
+        }
+        
+        //Get user data from code
+        /*if($_REQUEST['state'] == $_SESSION['state']) {*/
+            //Fetch data
+            $token_url = "https://graph.facebook.com/oauth/access_token?"
+            . "client_id=" . $app_id . "&redirect_uri=" . urlencode($my_url)
+            . "&client_secret=" . $app_secret . "&code=" . $code . "&scope=public_profile,email";
+
+            $response = @file_get_contents($token_url);
+            $params = null;
+            parse_str($response, $params);
+
+            $graph_url = "https://graph.facebook.com/me?access_token=" 
+            . $params['access_token'];
+
+            $user = json_decode(file_get_contents($graph_url));
+            
+            //Store the fetched info in the database
+            $this->fb_name = $user->name;
+            if(isset($user->email)){
+                $this->email = $user->email;
+            }else{
+                $this->email = '';
+            }
+            $this->facebook_id = $user->id;
+            
+            //Sjekk om brukeren er registrert
+            $stmt = $this->db->prepare("SELECT userID FROM `".$this->tbl['getmoving_user']."` WHERE facebookID = :facebookID");
+            $stmt->execute(array(
+                'facebookID' => $this->facebook_id
+            ));
+            if($res = $stmt->fetch(PDO::FETCH_ASSOC)){
+                return $this->login();
+            }
+            return $this->register();
+        /*}else{
+            echo("The state does not match. You may be a victim of CSRF.");
+        }*/
+        return false;
     }
     
     function login(){
         //Login user with username/pass or fb code
+        if(isset($this->facebook_id)){
+            //Facebook login
+            echo "Facebook login";
+            $stmt = $this->db->prepare("SELECT userID FROM `".$this->tbl['getmoving_user']."` WHERE facebookID = :facebookID");
+            $stmt->execute(array(
+                'facebookID' => $this->facebook_id
+            ));
+            if($res = $stmt->fetch(PDO::FETCH_ASSOC)){
+                //Make some kind of session system
+                $_SESSION['userID'] = $res['userID'];
+                return true;
+            }else{
+                //Troubleshooting
+                $this->error[] = "Didn't find user in database";
+            }
+        }else if(isset($_POST['username']) && isset($_POST['password'])){
+            echo "Normal login";
+        }
+        return false;
     }
     
     function register(){
         //Register new user
+        if(isset($this->fb_name) && isset($this->email) && isset($this->facebook_id)){
+            //Facebook register
+            echo "Facebook register";
+            
+            $datetime  = date('Y-m-d h:i:s');
+            
+            $stmt = $this->db->prepare("INSERT INTO `".$this->tbl['getmoving_user']."` 
+                (facebookID, username, name, email, register_datetime)
+                VALUES
+                (:facebookID, :username, :name, :email, :register_datetime)");
+            $ex = $stmt->execute(array(
+                'facebookID' => $this->facebook_id,
+                'username' => '',
+                'name' => $this->fb_name,
+                'email' => $this->email,
+                'register_datetime' => $datetime
+            ));
+            
+            if($ex){
+                //Logg inn the user
+                return $this->login();
+            }else{
+                //Troubeshoot
+                $this->error[] = "Failed to save user in db";
+            }
+        }else if(isset($_POST['username']) && isset($_POST['password'])){
+            echo "Normal register";
+        }
+        return false;
     }
 }
 }
--- main.php
+++ main.php
@@ -1,10 +1,52 @@
-﻿<?php 
-
+<?php
+/*
+ * Fetch user info if logged in
+ */
+if(isset($_SESSION['userID']) && is_numeric($_SESSION['userID'])){
+    $stmt = $db->prepare("SELECT * FROM `".$tbl['getmoving_user']."` WHERE userID = :userID");
+    $stmt->execute(array(
+        'userID' => $_SESSION['userID']
+    ));
+    if($userinfo = $stmt->fetch(PDO::FETCH_ASSOC)){
+        foreach($userinfo as $k => $v){ if($k == 'password'){continue;}
+            $vars['user_'.$k] = $v;
+        }
+    }
+}
+ 
 switch($page['type']){
     case "start":
         //Code for the start page
         
         break;
+    case "login":
+        //Redirect user to map page if already logged in
+        if(isset($_SESSION['userID']) && is_numeric($_SESSION['userID'])){
+            header("Location: /".get_pname($db, $tbl, "map").".html");
+        }
+        break;
+    case "handle_login":
+        //print_r($_REQUEST);
+        //$_SESSION['state'] = md5(uniqid(rand(), TRUE));  //CSRF protection
+        require CLIENT_CLASS_DIR.'login.class.php';
+        $login = new GM_Login();
+        $success = false;
+        if(isset($_GET['code'])){
+            //Facebook login
+            $success = $login->fb();
+        }else if(isset($_POST['username'])){
+            //Login
+        }else if(isset($_POST['password2'])){
+            
+        }
+        
+        if($success){
+            $url = "/".get_pname($db, $tbl, "map").".html";
+        }else{
+            $url = "/".get_pname($db, $tbl, "login")."/error=".implode(",", $login->error).".html";
+        }
+        header("Location: $url");
+        die("<script>window.location.href = '$url';</script>");
 }
 
 //Print header
@@ -12,7 +54,11 @@ $header = $template->getTemplateFile('header.html');
 $template->printTemplate($header, $perm_vars, $variables, $vars);
 
 //Print menu
-$menu = $template->getTemplateFile('menu.html');
+if(isset($_SESSION['userID'])){
+    $menu = $template->getTemplateFile('menu_loggedin.html');
+}else{
+    $menu = $template->getTemplateFile('menu.html');
+}
 $template->printTemplate($menu, $perm_vars, $variables, $vars);
 
 //Print page content
