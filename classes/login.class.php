29faf5a2522260790cf0ea3a27861db73e013760
Jonatan Norbye <jonatan@norbye.com>
2016-11-28 10:49:40 +0100

    Initialized GM_Login class

---
+++ classes/login.class.php
@@ -0,0 +1,20 @@
+<?php
+if(class_exists("GM_Login")){
+class GM_Login{
+    function __construct(){
+        //??
+    }
+    
+    function fb(){
+        //Get info from user account
+    }
+    
+    function login(){
+        //Login user with username/pass or fb code
+    }
+    
+    function register(){
+        //Register new user
+    }
+}
+}
