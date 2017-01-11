ea144a942add8ab815f60e1b7eee3a01585c5578
Jonatan Norbye <jonatan@norbye.com>
2017-01-11 10:24:12 +0100

    Started working on the welcome page

---
+++ clients/getmoving/main.php
@@ -0,0 +1,25 @@
+﻿<?php 
+
+switch($page['type']){
+    case "start":
+        //Code for the start page
+        
+        break;
+}
+
+//Print header
+//$header = $template->getTemplateFile('header.html');
+//$template->printTemplate($header, $perm_vars, $variables, $vars);
+
+//Print menu
+//$menu = $template->getTemplateFile('menu.html');
+//$template->printTemplate($menu, $perm_vars, $variables, $vars);
+
+//Print page content
+$template->printTemplate($content, $perm_vars, $variables, $vars);
+
+//Print footer
+//$footer = $template->getTemplateFile('footer.html');
+//$template->printTemplate($footer, $perm_vars, $variables, $vars);
+
+?>
