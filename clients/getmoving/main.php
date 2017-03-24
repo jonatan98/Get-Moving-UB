06cd02b3d0dfa953f38afb92207c96d36056577d
Jonatan Norbye <jonatan@norbye.com>
2017-03-24 16:40:03 +0100

    Various updates

--- clients/getmoving/main.php
+++ clients/getmoving/main.php
@@ -5,6 +5,19 @@ switch($page['type']){
         //Code for the start page
         
         break;
+    case "reklamefilm":
+        $file = __DIR__ . '/click.log';
+        // Open the file to get existing content
+        $current = file_get_contents($file);
+        // Append a new person to the file
+        $_SERVER['HTTP_REFERER'] = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+        $current .= date("Y-m-d H:i:s") . " " . $_SERVER['REMOTE_ADDR'] . " " . $_SERVER['HTTP_REFERER'] . "\n";
+        // Write the contents back to the file
+        file_put_contents($file, $current);
+        $url = "https://youtu.be/Xgs9RPmDJUE";
+        header("Location: $url");
+        die("<script>window.location.href = '$url';</script>");
+        break;
 }
 
 //Print header
